7112862166055514ff3f9341effd9be3750762d8
naom0649 <naom0649@stud.kre.dk>
2022-12-16 10:29:50 +0100

    liste til control og linse fornyelse design og lidt ændringer i de andre klasser

--- PSBProjekt/lensStrength.js
+++ PSBProjekt/lensStrength.js
@@ -1,5 +1,5 @@
 
-fetch("/json/personlig.json")
+fetch("/PSBProjekt/øjne.json")
 
 
 
@@ -13,29 +13,65 @@ fetch("/json/personlig.json")
 
     const id = card.querySelector("[data-id]")
 
-    const customerName = card.querySelector("[data-customerName]")
 
-    const phoneNumber = card.querySelector("[data-phoneNumber]")
+    const lstrength = card.querySelector("[data-lstrength]")
 
-    const startDate = card.querySelector("[data-startDate]")
+    const rstrength = card.querySelector("[data-rstrength]")
 
-    const endDate = card.querySelector("[data-endDate]")
+    const lradius = card.querySelector("[data-lradius]")
 
-    const status = card.querySelector("[data-status]")
+    const rradius = card.querySelector("[data-rradius]")
 
-    id.textContent = booking.id,customerName.textContent = booking.customerName, phoneNumber.textContent = booking.phoneNumber, startDate.textContent = booking.startDate,
+    const ldiameter = card.querySelector("[data-ldiameter]")
 
-    endDate.textContent = booking.endDate, status.textContent = booking.status
+    const rdiameter = card.querySelector("[data-rdiameter]")
+
+    const lcylinder = card.querySelector("[data-lcylinder]")
+
+    const rcylinder = card.querySelector("[data-rcylinder]")
+
+    const laxe = card.querySelector("[data-laxe]")
+
+    const raxe = card.querySelector("[data-raxe]")
+
+    const laddition = card.querySelector("[data-laddition]")
+
+    const raddition = card.querySelector("[data-raddition]")
+
+    const lfrequency = card.querySelector("data-lfrequency")
+
+    const rfrequency = card.querySelector("data-rfrequency")
+
+    const lsupplier = card.querySelector("data-lsupplier")
+    
+    const rsupplier = card.querySelector("data-rsupplier")
+
+    const lpackettype = card.querySelector("data-lpackettype")
+
+    const rpackettype = card.querySelector("data-packettype")
+
+    const lamount = card.querySelector("data-lamount")
+
+    const ramount = card.querySelector("data-ramount")
+
+    const customerName = card.querySelector("data-customerName")
+
+
+
+    
+
+
+    id.textContent = booking.id,lstrength.textContent = booking.lstrength, rstrength.textContent = booking.rstrength, lradius.booking = booking.lradius,
+    rradius.textContent = booking.rradius,ldiameter.textContent = booking.ldiameter,rdiameter.textContent = booking.rdiameter,lcylinder.textContent = booking.lcylinder,
+    rcylinder.textContent = booking.rcylinder,laxe.textContent = booking.laxe,raxe.textContent = booking.raxe,laddition.textContent = booking.laddition,
+    raddition.textContent = booking.raddition,lfrequency.textContent = booking.lfrequency,rfrequency.textContent = booking.rfrequency,
+    lsupplier.textContent = booking.lsupplier, rsupplier.textContent = booking.rsupplier,lpackettype.textContent = booking.lpackettype,rpackettype.textContent = booking.rpackettype,
+    lamount.textContent = booking.lamount,ramount.textContent = booking.ramount,customerName.textContent = booking.customerName
+    
 
     bookingCardContainer.append(card)
 
-    return {
-
-        id: booking.id, customerName: booking.customerName, phoneNumber: booking.phoneNumber, mail: booking.mail, CPR: booking.CPR,
-
-        startDate: booking.startDate, endDate: booking.endDate, status: booking.status,
-
-        element: card }
+   
 
 })  
 
